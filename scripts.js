ef4ccadc340f16698192ba51372dd22871b9b0a1
mtali <emmanuelmtali@yahoo.com>
2019-07-01 17:57:21 +0300

    Create PeopleController

---
+++ scripts.js
@@ -0,0 +1,7 @@
+function PeopleController($scope) {
+    $scope.people = [
+        {name: "Emmanuel S Mtali", phone: "255719526215", city: "Dar es Salaam"},
+        {name: "Agatha Kikoti", phone: "255765585581", city: "Iringa"},
+        {name: "Christopha Mtandala", phone: "25578689967", city: "Morogoro"}
+    ];
+}
