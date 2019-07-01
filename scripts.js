e261a1c236b42d4f925bda08d3e77af25d56cbd0
mtali <emmanuelmtali@yahoo.com>
2019-07-01 18:34:12 +0300

    Implement adding contact with 2 way data binding feature

--- scripts.js
+++ scripts.js
@@ -4,4 +4,12 @@ function PeopleController($scope) {
         {name: "Agatha Kikoti", phone: "255765585581", city: "Iringa"},
         {name: "Christopha Mtandala", phone: "25578689967", city: "Morogoro"}
     ];
+
+    $scope.save = function() {
+        $scope.people.push({
+            name: $scope.newPerson.name,
+            phone: $scope.newPerson.phone,
+            city: $scope.newPerson.city
+        });
+    }
 }
